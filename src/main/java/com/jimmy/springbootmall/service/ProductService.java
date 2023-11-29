3eec39837cbb510e3507bf92a518b4fb23e670d9
jimmy <jimmymomo123@gmail.com>
2023-11-29 18:09:19 +0800

    商品功能 - 實作查詢商品列表

--- src/main/java/com/jimmy/springbootmall/service/ProductService.java
+++ src/main/java/com/jimmy/springbootmall/service/ProductService.java
@@ -3,11 +3,12 @@ package com.jimmy.springbootmall.service;
 import com.jimmy.springbootmall.dto.ProductRequest;
 import com.jimmy.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
-
     Integer createProduct(ProductRequest productRequest);
-
     void updateProduct(Integer productId,ProductRequest productRequest);
     void deleteProductById(Integer productId);
 }
